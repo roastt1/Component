6c436e8f6127ad2dc5510e31deb786b6f9601ac3
roast <roastt1@naver.com>
2025-04-10 00:11:33 +0900

    test: 테스트해봄

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
+'use client';
 import Button from "./components/Button";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const handleClick = () => {
+    alert("버튼 클릭됨");
+  };
   return (
     <div className={styles.page}>
-      <Button variant="primary" size="small">기본</Button>
-      <Button variant="save" size="medium">저장하기</Button>
-      <Button variant="cancle" size="large">취소하기</Button>
-      <Button variant="primary" size="small" isAbled>기본</Button>
-      <Button variant="save" size="medium" isAbled>저장하기</Button>
-      <Button variant="cancle" size="large" isAbled>취소하기</Button>
+      <Button variant="primary" size="sm" onClick={handleClick}>기본</Button>
+      <Button variant="save" size="md">저장하기</Button>
+      <Button variant="cancel" size="lg">취소하기</Button>
+      <Button variant="primary" size="sm">기본</Button>
+      <Button variant="save" size="md">저장하기</Button>
+      <Button variant="cancel" size="lg" disabled>취소하기</Button>
     </div>
   );
 }
