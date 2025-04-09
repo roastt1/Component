a374b7f2a95bd91a0cba2d1f5224d62c5698cdc9
roast <roastt1@naver.com>
2025-04-10 00:07:50 +0900

    docs: 주석추가

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,25 +2,23 @@ import React, { FC, ButtonHTMLAttributes } from "react";
 import styles from "./Button.module.scss";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  variant?: "primary" | "save" | "cancle"; // 버튼 스타일 종류
-  size?: "small" | "medium" | "large"; // 버튼 크기
-  isAbled?: boolean; // 비활성화 여부
+  variant?: "primary" | "save" | "cancel"; // 버튼 스타일 종류
+  size?: "sm" | "md" | "lg"; // 버튼 크기
+  isDisabled?: boolean; // 비활성화 여부
 };
 
 const Button: FC<ButtonProps> = ({
   variant = "primary",
   size = "medium",
-  isAbled = false,
-  children,
-  ...props
+  children, // 버튼 텍스트
+  ...props// 나머지 props ex) onClick등
 }) => {
   return (
     <button
-      className={`${styles.button} ${styles[variant]} ${styles[size]}`}
-      disabled={isAbled || props.disabled}
-      {...props}
+      className={`${styles.button} ${styles[variant]} ${styles["button_"+size]}`}
+      {...props} // 나머지 props 전달
     >
-      {isAbled ? "disabled" : children}
+      {children}
     </button>
   );
 };
