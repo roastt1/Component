da0ebc929a8280734a0787ce12a90e76bca403e3
roast <roastt1@naver.com>
2025-04-14 21:30:39 +0900

    '작업 중인 변경사항 커밋'

--- app/page.tsx
+++ app/page.tsx
@@ -59,6 +59,32 @@ export default function Home() {
             </div>
           }
         ></Card>
+        <Card
+          header={
+            <span className={`${styles.category} ${styles["프로젝트"]}`}>
+              프로젝트
+            </span>
+          }
+          body={
+            <>
+              <div className={styles.deadline}>마감일 | 2025.04.17</div>
+              <div className={styles.title}>나랑 프로젝트 할 사람!</div>
+            </>
+          }
+          footer={
+            <div className={styles.userInfo}>
+              <div className={styles.profileImageContainer}>
+                <img
+                  src="https://codingapple-cdn.b-cdn.net/wp-content/uploads/2023/01/food2.png"
+                  alt="프로필 이미지"
+                  className={styles.profileImage}
+                />
+              </div>
+              <span>박준선</span>
+              <span>❤️ 좋아요 30</span>
+            </div>
+          }
+        ></Card>
       </div>
       <CountStoreComponent />
     </>
