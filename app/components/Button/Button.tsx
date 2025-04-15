ebb7c72b12ee4bf093e1d814465363dc14353f43
roast <roastt1@naver.com>
2025-04-16 00:21:09 +0900

    refactor: "버튼 리팩토링"

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -3,21 +3,18 @@ import styles from "./Button.module.scss";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: "primary" | "save" | "cancel"; // 버튼 스타일 종류
-  size?: "sm" | "md" | "lg"; // 버튼 크기
-  isDisabled?: boolean; // 비활성화 여부
-  fullWidth?: boolean; // 가로 전체 사용 여부
+  size?: "sm" | "md" | "lg" | 'long'; // 버튼 크기
 };
 
 const Button: FC<ButtonProps> = ({
-  variant = "primary",
-  size = "medium",
-  fullWidth = false,
+  variant="primary", // 기본값 primary
+  size,
   children, // 버튼 텍스트
   ...props// 나머지 props ex) onClick등
 }) => {
   return (
     <button
-      className={`${styles.button} ${styles[variant]} ${styles["button_"+size]} ${fullWidth ? styles.fullWidth : ""}`}
+      className={`${styles.button} ${styles[variant]} ${styles["button_"+size]}`}
       {...props} // 나머지 props 전달
     >
       {children}
--- app/components/Button/page.tsx
+++ app/components/Button/page.tsx
@@ -8,17 +8,26 @@ export default function Home() {
   return (
     <>
       <div>
-        <Button variant="primary" size="sm" onClick={handleClick}>
+        <Button variant="primary" size="long" onClick={handleClick}>
           기본
         </Button>
         <Button variant="save" size="md">
           저장하기
         </Button>
-        <Button variant="cancel" size="lg">
+        <Button variant="cancel" size="md">
           취소하기
         </Button>
-        <Button variant="cancel" size="lg" disabled>
-          취소하기
+        <Button variant="cancel" size="lg">
+          이전
+        </Button>
+        <Button variant="primary" size="lg">
+          다음
+        </Button>
+        <Button variant="cancel" size="sm">
+          거절하기
+        </Button>
+        <Button variant="save" size="sm">
+          수락하기
         </Button>
       </div>
     </>
